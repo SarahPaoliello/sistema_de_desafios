a283ea174a69add41558a2b81ac69c40ba0960cd
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-18 15:29:34 -0300

    Update votacao.py

--- telas/votacao.py
+++ telas/votacao.py
@@ -1,1 +1,34 @@
+import streamlit as st
 
+from services.voto_service import votar
+
+
+def tela_votacao():
+
+    desafio = st.session_state.desafio
+
+    st.title("🗳️ Votação")
+
+    st.subheader(desafio["titulo"])
+
+    voto = st.radio(
+        "Escolha",
+        ["Bom", "Regular", "Ruim"]
+    )
+
+    participante_id = 1
+
+    if st.button("Enviar voto"):
+
+        resultado = votar(
+            st.session_state.usuario["id"],
+            participante_id,
+            voto
+        )
+
+        if resultado == "ok":
+
+            st.success("Voto registrado")
+
+        else:
+            st.warning(resultado)
