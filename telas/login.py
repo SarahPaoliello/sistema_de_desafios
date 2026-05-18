f203eb1a8c5f63c4eff86c5edc1e8099358a24f2
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-18 15:28:50 -0300

    Update login.py

--- telas/login.py
+++ telas/login.py
@@ -1,1 +1,30 @@
+import streamlit as st
 
+from services.auth_service import login
+
+
+def tela_login():
+
+    st.title("🔑 Login")
+
+    email = st.text_input("Email")
+
+    senha = st.text_input(
+        "Senha",
+        type="password"
+    )
+
+    if st.button("Entrar"):
+
+        usuario = login(email, senha)
+
+        if usuario:
+
+            st.session_state.usuario = usuario
+
+            st.success("Login realizado!")
+
+            st.rerun()
+
+        else:
+            st.error("Credenciais inválidas")
