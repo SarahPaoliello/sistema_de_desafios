4513e48c80982e170090e8102d45e79681a4cf22
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-18 15:54:54 -0300

    Update conexao.py

--- database/conexao.py
+++ database/conexao.py
@@ -1,26 +1,7 @@
 from supabase import create_client
 import streamlit as st
 
-try:
+url = st.secrets["SUPABASE_URL"]
+key = st.secrets["SUPABASE_KEY"]
 
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
-
-    st.write("URL:", url)
-
-    supabase = create_client(url, key)
-
-    teste = (
-        supabase
-        .table("usuarios")
-        .select("*")
-        .limit(1)
-        .execute()
-    )
-
-    st.success("Supabase conectado!")
-
-except Exception as e:
-
-    st.error("ERRO NA CONEXÃO")
-    st.exception(e)
+supabase = create_client(url, key)
