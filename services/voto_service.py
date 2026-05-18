b83e6c47bdb4084ad0c5c50f68caaba55edbaa01
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-18 15:28:25 -0300

    Update voto_service.py

--- services/voto_service.py
+++ services/voto_service.py
@@ -1,1 +1,23 @@
+from database.conexao import supabase
 
+
+def votar(usuario_id, participante_id, voto):
+
+    existe = (
+        supabase.table("votos")
+        .select("id")
+        .eq("usuario_id", usuario_id)
+        .eq("participante_id", participante_id)
+        .execute()
+    )
+
+    if existe.data:
+        return "Você já votou"
+
+    supabase.table("votos").insert({
+        "usuario_id": usuario_id,
+        "participante_id": participante_id,
+        "voto": voto
+    }).execute()
+
+    return "ok"
