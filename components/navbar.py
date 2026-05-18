fe3d69c6cd066698f554f6df3eb1616102babff4
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-18 15:33:26 -0300

    Update navbar.py

--- components/navbar.py
+++ components/navbar.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+
+def navbar():
+
+    st.sidebar.title("Sistema")
+
+    pagina = st.sidebar.radio(
+        "Menu",
+        [
+            "Home",
+            "Desafios",
+            "Mini Provas"
+        ]
+    )
+
+    return pagina
