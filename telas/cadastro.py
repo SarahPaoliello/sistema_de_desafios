f6b18c9c73a2a304be0efb210c57b68ff92346eb
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-18 15:43:36 -0300

    Update cadastro.py

--- telas/cadastro.py
+++ telas/cadastro.py
@@ -7,26 +7,38 @@ def tela_cadastro():
 
     st.title("Cadastro")
 
-    nome = st.text_input("Nome")
+    nome = st.text_input(
+        "Nome",
+        key="cad_nome"
+    )
 
-    email = st.text_input("Email")
+    email = st.text_input(
+        "Email",
+        key="cad_email"
+    )
 
     tipo = st.selectbox(
         "Tipo",
-        ["aluno", "professor"]
+        ["aluno", "professor"],
+        key="cad_tipo"
     )
 
     senha = st.text_input(
         "Senha",
-        type="password"
+        type="password",
+        key="cad_senha"
     )
 
     confirmar = st.text_input(
         "Confirmar senha",
-        type="password"
+        type="password",
+        key="cad_confirmar"
     )
 
-    if st.button("Cadastrar"):
+    if st.button(
+        "Cadastrar",
+        key="btn_cadastro"
+    ):
 
         if senha != confirmar:
             st.error("Senhas diferentes")
